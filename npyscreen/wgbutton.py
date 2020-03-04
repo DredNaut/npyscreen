e206bf19b62792c3e28efb1b256119c3ef9f2c53
Jared Knutson <jaredknutson@nevada.unr.edu>
2020-03-04 15:44:49 -0800

    Update wgbutton.py

--- npyscreen/wgbutton.py
+++ npyscreen/wgbutton.py
@@ -23,18 +23,6 @@ class MiniButton(checkbox._ToggleControl):
         else:
             self.color = 'CONTROL'
         
-    def set_up_handlers(self):
-        super(MiniButton, self).set_up_handlers()
-        
-        self.handlers.update({
-                curses.KEY_ENTER: self.h_toggle,
-                curses.ascii.SP:    self.h_toggle,
-                curses.KEY_DOWN:     self.h_exit_down,
-                curses.KEY_UP:       self.h_exit_up,
-                curses.ascii.NL:     self.h_exit_down,
-                curses.ascii.CR:     self.h_exit_down,
-            })
-        
     def calculate_area_needed(self):
         return 1, self.label_width+2
 
